87fb254c2528ae79f492052849fefb4fa2614047
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-03-30 10:05:53 +0100

    Add chat response functionality
    The changes in this commit introduce a new chat response functionality to the existing application. The main changes are:
    
    1. Added new import statements for `futures`, `hiramu` modules, and `tokio`.
    2. Implemented a new `chat_response_loop` function that handles the chat response flow.
    3. Added a `print_and_get_chat_response` function to handle the printing and retrieval of the chat response.
    4. Removed the previous `generate_response_loop` function and replaced it with a new `print_generate_response` function to handle the printing of the generate response.
    5. The `main` function now calls the new `chat_response_loop` function instead of the `generate_response_loop`.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
-use hiramu::ollama::models::GenerateRequestBuilder;
-use hiramu::ollama::ollama_client::OllamaClient;
-use std::io::{self, Write};
-use tokio;
+use futures::stream::TryStream; // Add this import statement
 use futures_util::TryStreamExt;
+use hiramu::ollama::models::ChatRequestBuilder;
+use hiramu::ollama::models::ChatResponse;
+use hiramu::ollama::models::GenerateRequestBuilder;
+use hiramu::ollama::models::Message;
+use hiramu::ollama::ollama_client::FetchStreamError;
+use hiramu::ollama::ollama_client::OllamaClient;
+use hiramu::GenerateResponse;
+use std::io::{self, Write};
+use tokio; // Add this import statement
 
 #[tokio::main]
 async fn main() {
+    // generate_response_loop().await;
+    chat_response_loop().await;
+}
+
+async fn chat_response_loop() {
+    let client = OllamaClient::new("http://localhost:11434".to_string());
+
+    let mut messages = Vec::new();
+
+    loop {
+        let input = prompt_input("\nUser: ").unwrap();
+        messages.push(Message {
+            role: "user".to_string(),
+            content: input,
+            images: vec![],
+        });
+
+        let request = ChatRequestBuilder::new("mistral".to_string())
+            .messages(messages.clone())
+            .build();
+
+        let response_stream = client.chat(request).await.unwrap();
+
+        let response = print_and_get_chat_response(response_stream).await.unwrap();
+
+        // get last response from the chat
+
+        messages.push(Message {
+            role: "assistant".to_string(),
+            content: response,
+            images: vec![],
+        });
+    }
+}
+
+async fn generate_response_loop() {
     let client = OllamaClient::new("http://localhost:11434".to_string());
 
     loop {
@@ -16,18 +58,7 @@ async fn main() {
 
         let response = client.generate(request).await.unwrap();
 
-
-        response
-            .try_for_each(|chunk| async {
-                let response = chunk.response;
-                print!("{}", response);
-                // Flush the output to ensure the prompt is displayed.
-                io::stdout().flush().unwrap();
-                Ok(())
-            })
-            .await
-            .unwrap();        
-
+        print_generate_response(response).await.unwrap();
     }
 }
 
@@ -37,4 +68,35 @@ fn prompt_input(prompt: &str) -> Result<String, std::io::Error> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     Ok(input.trim().to_string())
+}
+
+async fn print_and_get_chat_response(
+    response: impl TryStream<Ok = ChatResponse, Error = FetchStreamError>,
+) -> Result<String, FetchStreamError> {
+    let words = response
+        .try_fold(String::new(), |mut f, chunk| async {
+            let response = chunk.message.content;
+            print!("{}", response);
+            io::stdout().flush().unwrap();
+            f.push_str(&response);
+            Ok(f)
+        })
+        .await
+        .unwrap();
+
+    Ok(words)
+}
+
+async fn print_generate_response(
+    response: impl TryStream<Ok = GenerateResponse, Error = FetchStreamError>,
+) -> Result<(), FetchStreamError> {
+    response
+        .try_for_each(|chunk| async {
+            let response = chunk.response;
+            print!("{}", response);
+            // Flush the output to ensure the prompt is displayed.
+            io::stdout().flush().unwrap();
+            Ok(())
+        })
+        .await
 }
